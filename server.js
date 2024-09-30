bf2d805d54558a13271fd5ccd843fded984573bd
VidAbw <vidushaupashaly@gmail.com>
2024-09-30 23:32:06 +0530

    3rd push

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import express from "express";
 import colors from "colors";
+import dotenv from 'dotenv'
+
+//configure env 
+dotenv.config();
 //rest object
 const app = express()
 
@@ -9,9 +13,9 @@ app.get('/', (req,res) =>{
 })
 
 //PORT
-const PORT = 8080
+const PORT = process.env.PORT || 8080;
 
 // run listen
 app.listen(PORT,() =>{
-    console.log(`Server Running on ${PORT}`.bgCyan.white);
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 });
